3a2a1569559372af0bb80fc8114bcb7144e98e15
georgy.vandyshev <georgy.vandyshev@inslab.ru>
2021-04-26 14:56:19 +0300

    Доработал Можно лучше, кроме липкого футера и auto/fit minmax

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
-let formElement = document.querySelector(".popup__content"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__input_control_name");// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".popup__input_control_job");// Воспользуйтесь инструментом .querySelector()
+let formElement = document.querySelector(".popup__content-form");         // Форма редактирования профиля
+let nameInput = formElement.querySelector(".popup__input_control_name");  // Текстовое поле ввода имени профиля
+let jobInput = formElement.querySelector(".popup__input_control_job");    // Текстовое поле ввода описания/профессии профиля
 
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__description");
+let profileName = document.querySelector(".profile__name");               // Элемент с именем профиля на странице
+let profileJob = document.querySelector(".profile__description");         // Элемент с описанием/профессией профиля на странице
 
-let profileEditBtn = document.querySelector(".profile__edit-button");
-let closeBtn = document.querySelector(".popup__close-btn");
+let profileEditBtn = document.querySelector(".profile__edit-button");     // Кнопка "Редактировать профиль"
+let closeBtn = document.querySelector(".popup__close-btn");               // Кнопка "Закрыть попап"
 
-let popupSection = document.querySelector(".popup");
+let popupSection = document.querySelector(".popup");                      // Элемент попап
 
-
-
-function popupToggle(evt) {
+//Функция открытия попапа
+function openPopup(evt){
   evt.preventDefault();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popupSection.classList.toggle("popup_hidden");
-  if(evt.currentTarget.className === "profile__edit-button"){
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-  }
+}
+
+//Функция закрытия попапа
+function closePopup(evt){
+  if(evt)
+    evt.preventDefault();
+  popupSection.classList.toggle("popup_hidden");
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -28,12 +31,15 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupToggle(evt);
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
-profileEditBtn.addEventListener('click', popupToggle);
-closeBtn.addEventListener('click', popupToggle);
+//Нажатие кнопки "Редактировать профиль"
+profileEditBtn.addEventListener('click', openPopup);
+
+//Нажатие кнопки "Закрыть попап"
+closeBtn.addEventListener('click', closePopup);
